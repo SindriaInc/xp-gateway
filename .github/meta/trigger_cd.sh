f73a23ee01b9e637dd770717b9cada35269e4795
lucapitzoi <luca.pitzoi@sindria.org>
2022-03-13 20:51:50 +0100

    Fix trigger cd

--- .github/meta/trigger_cd.sh
+++ .github/meta/trigger_cd.sh
@@ -13,11 +13,11 @@ fi
 IAC_REPO_SLUG_REF=$1
 IAC_RELEASE_VERSION_REF=$2
 
-JSON_PAYLOAD=$( jq -n \
-                  --arg iac_repo_slug_ref "${IAC_REPO_SLUG_REF}" \
-                  --arg iac_release_version_ref "${IAC_RELEASE_VERSION_REF}" \
-'{"target": {"ref_type": "branch","type": "pipeline_ref_target","ref_name": "master"},"variables": [{"key": "IAC_REPO_SLUG_REF","value": $iac_repo_slug_ref,"secured": "false"},{"key": "IAC_RELEASE_VERSION_REF","value": $iac_release_version_ref,"secured": "false"}]}')
+JSON_PAYLOAD=$(jq --tab -cn --arg iac_repo_slug_ref "${IAC_REPO_SLUG_REF}" --arg iac_release_version_ref "${IAC_RELEASE_VERSION_REF}" '{"target": {"ref_type": "branch","type": "pipeline_ref_target","ref_name": "master"}, "variables": [{"key": "IAC_REPO_SLUG_REF","value": $iac_repo_slug_ref,"secured": false},{"key": "IAC_RELEASE_VERSION_REF","value": $iac_release_version_ref,"secured": false}]}' --)
 
 echo ${JSON_PAYLOAD}
+echo
+echo "https://api.bitbucket.org/2.0/repositories/${IAC_GIT_NAMESPACE}/${IAC_GIT_REPO_SLUG}/pipelines/"
+echo
 
-curl -X POST -is -u ${IAC_GIT_USERNAME}:${IAC_GIT_PASSWORD} -H 'Content-Type: application/json' https://api.bitbucket.org/2.0/repositories/${IAC_GIT_NAMESPACE}/${IAC_GIT_REPO_SLUG}/pipelines -d ${JSON_PAYLOAD}
+curl -X POST -is -u ${IAC_GIT_USERNAME}:${IAC_GIT_PASSWORD} -H 'Content-Type: application/json' https://api.bitbucket.org/2.0/repositories/${IAC_GIT_NAMESPACE}/${IAC_GIT_REPO_SLUG}/pipelines/ -d ${JSON_PAYLOAD}
